7577b661fa1c32eabc7635ddf8950a29c751641c
Jaziel96 <jazcov96@hotmail.com>
2022-07-26 03:24:29 +0000

    pagina react

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function (){
+    
+return (
+<div className="container-fluid">
+					<footer className="py-3 my-4 bg-dark">
+						<p className="text-center text-white">CopyRight© 2021 Company, Inc</p>
+					</footer>
+					</div>
+					
+					
+);
+}
